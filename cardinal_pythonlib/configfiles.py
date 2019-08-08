ff2ff05afd83059f7dbd63a85413d7ef20acbf6d
Rudolf Cardinal <rudolf@pobox.com>
2019-08-09 00:39:38 +0100

    docstring typo

--- cardinal_pythonlib/configfiles.py
+++ cardinal_pythonlib/configfiles.py
@@ -149,7 +149,7 @@ def get_config_bool_option(parser: ConfigParser,
         default: value to return if option is absent
 
     Returns:
-        string value
+        Boolean value
 
     Raises:
         ValueError: if the section is absent
