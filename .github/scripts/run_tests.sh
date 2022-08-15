d7690e7d424dce9dfd2d5597a87fea3b25bfdd06
Martin Burchell <mb2353@cam.ac.uk>
2022-08-15 12:02:56 +0100

    Install pytest

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -14,6 +14,7 @@ python -m pip install "numpy<1.23"  # 1.23 incompatible with numba
 python -m pip install xlrd
 python -m pip install dogpile.cache==0.9.2  # Later versions incompatible
 python -m pip install "SQLAlchemy<1.4"  # _get_immediate_cls_attr moved in 1.4
+python -m pip install pytest
 python -m pip install -e .
 
 pytest
