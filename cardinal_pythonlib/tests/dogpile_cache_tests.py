02d71b3d145199f5e7e2be81bb93875eebdb0cf4
Martin Burchell <mb2353@cam.ac.uk>
2023-01-24 22:01:09 +0000

    Black

--- cardinal_pythonlib/tests/dogpile_cache_tests.py
+++ cardinal_pythonlib/tests/dogpile_cache_tests.py
@@ -104,9 +104,7 @@ class DogpileCacheTests(unittest.TestCase):
             """
             nonlocal fn_was_called
             log.info(result)
-            assert (
-                fn_was_called == should_call_fn
-            ), (
+            assert fn_was_called == should_call_fn, (
                 f"fn_was_called={fn_was_called}, "
                 f"should_call_fn={should_call_fn}"
             )
