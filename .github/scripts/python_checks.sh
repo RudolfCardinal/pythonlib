f3b230550cd83d505fd3130276e498b04144392e
Martin Burchell <mb2353@cam.ac.uk>
2023-01-24 16:29:37 +0000

    Run python checks in GitHub workflow

---
+++ .github/scripts/python_checks.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Run from .github/workflows/python_checks.yml
+
+set -eux -o pipefail
+
+python3 -m venv "${HOME}/venv"
+source "${HOME}/venv/bin/activate"
+python -m site
+python -m pip install -U pip
+
+echo installing pip packages
+python -m pip install -e .
+
+echo running pre-commit checks
+pre-commit run --all-files
