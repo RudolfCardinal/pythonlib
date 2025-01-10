7b4afde4edb65587411173f16f67ece0321c496c
Martin Burchell <mb2353@cam.ac.uk>
2025-01-10 11:39:54 +0000

    Workflow refactoring to use common python setup

--- .github/scripts/install_base_python_packages.sh
+++ .github/scripts/install_base_python_packages.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# Run from .github/workflows/tests.yml
-
 set -euo pipefail
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_ROOT_DIR=${THIS_DIR}/../..
 VENV_DIR=${HOME}/venv
-PYTEST=${VENV_DIR}/bin/pytest
 
-sudo apt-get install wkhtmltopdf
-
-cd "${PROJECT_ROOT_DIR}"
-
-# pytest --log-cli-level=INFO
-${PYTEST}
+PYTHON=${VENV_DIR}/bin/python
+echo Installing pip packages
+${PYTHON} -m pip install -e "${PROJECT_ROOT_DIR}"
--- .github/scripts/python_checks.sh
+++ .github/scripts/python_checks.sh
@@ -2,16 +2,19 @@
 
 # Run from .github/workflows/python_checks.yml
 
-set -eux -o pipefail
+set -euo pipefail
 
-python3 -m venv "${HOME}/venv"
-source "${HOME}/venv/bin/activate"
-python -m site
-python -m pip install -U pip setuptools
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_ROOT_DIR=${THIS_DIR}/../..
+VENV_DIR=${HOME}/venv
+PYTHON=${VENV_DIR}/bin/python
+PRECOMMIT=${VENV_DIR}/bin/pre-commit
 
-echo installing pip packages
-python -m pip install -e .
-python -m pip install pre-commit
+cd "${PROJECT_ROOT_DIR}"
 
-echo running pre-commit checks
-pre-commit run --all-files
+echo Installing pre-commit
+${PYTHON} -m pip install -e .
+${PYTHON} -m pip install pre-commit
+
+echo Running pre-commit checks
+${PRECOMMIT} run --all-files
