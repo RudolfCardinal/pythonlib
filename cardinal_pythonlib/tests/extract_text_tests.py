21e2b81c03aa8927d996af0f092cab5085cab6cf
Martin Burchell <mb2353@cam.ac.uk>
2025-04-30 08:54:15 +0100

    Test PDF file conversion

--- cardinal_pythonlib/tests/extract_text_tests.py
+++ cardinal_pythonlib/tests/extract_text_tests.py
@@ -27,12 +27,13 @@
 
 import os
 import subprocess
-from tempfile import TemporaryDirectory, NamedTemporaryFile
+from tempfile import mkdtemp, NamedTemporaryFile
 from unittest import mock, TestCase
 
 from faker import Faker
 from faker_file.providers.docx_file import DocxFileProvider
 from faker_file.providers.odt_file import OdtFileProvider
+from faker_file.providers.pdf_file import PdfFileProvider
 
 from cardinal_pythonlib.extract_text import (
     document_to_text,
@@ -42,17 +43,15 @@ from cardinal_pythonlib.extract_text import (
 
 
 class DocumentToTextTests(TestCase):
-    # For external tools we assume the tools are running correctly
-    # and we just check that they are invoked with the correct arguments.
     def setUp(self) -> None:
-        update_external_tools(
-            {
-                "antiword": "/path/to/antiword",
-            }
-        )
+        self.empty_dir = mkdtemp()
 
+        self._replace_external_tools_with_fakes()
         self.config = TextProcessingConfig()
+        self._create_mock_objects()
+        self._register_faker_providers()
 
+    def _create_mock_objects(self) -> None:
         # Some mock empty output that we don't check
         mock_decode = mock.Mock(return_value="")
         mock_stdout = mock.Mock(decode=mock_decode)
@@ -61,9 +60,29 @@ class DocumentToTextTests(TestCase):
             return_value=mock.Mock(communicate=mock_communicate)
         )
 
+    def _register_faker_providers(self) -> None:
         self.fake = Faker()
         self.fake.add_provider(DocxFileProvider)
         self.fake.add_provider(OdtFileProvider)
+        self.fake.add_provider(PdfFileProvider)
+
+    def _replace_external_tools_with_fakes(self) -> None:
+        # For external tools we assume the tools are running correctly
+        # and we just check that they are invoked with the correct arguments.
+
+        tool_names = [
+            "antiword",
+            "pdftotext",
+            "strings",
+            "strings2",
+            "unrtf",
+        ]
+
+        tools_dir = {t: os.path.join(self.empty_dir, t) for t in tool_names}
+        update_external_tools(tools_dir)
+
+    def tearDown(self) -> None:
+        os.rmdir(self.empty_dir)
 
     def test_raises_when_no_filename_or_blob(self) -> None:
         with self.assertRaises(ValueError) as cm:
@@ -85,9 +104,8 @@ class DocumentToTextTests(TestCase):
 
     def test_raises_when_not_a_file(self) -> None:
         with self.assertRaises(ValueError) as cm:
-            with TemporaryDirectory() as temp_dir_name:
-                filename = os.path.join(temp_dir_name, "foo")
-                document_to_text(filename=filename)
+            filename = os.path.join(self.empty_dir, "foo")
+            document_to_text(filename=filename)
 
         self.assertIn("no such file", str(cm.exception))
 
@@ -113,7 +131,7 @@ class DocumentToTextTests(TestCase):
         expected_calls = [
             mock.call(
                 (
-                    "/path/to/antiword",
+                    f"{self.empty_dir}/antiword",
                     "-w",
                     str(self.config.width),
                     temp_file.name,
@@ -135,7 +153,7 @@ class DocumentToTextTests(TestCase):
         expected_calls = [
             mock.call(
                 (
-                    "/path/to/antiword",
+                    f"{self.empty_dir}/antiword",
                     "-w",
                     str(self.config.width),
                     temp_file.name,
@@ -193,3 +211,24 @@ class DocumentToTextTests(TestCase):
         text = document_to_text(odt.data["filename"], config=self.config)
 
         self.assertEqual(text.strip(), content)
+
+    def test_pdf_converted(self) -> None:
+        with mock.patch.multiple(
+            "cardinal_pythonlib.extract_text.subprocess",
+            Popen=self.mock_popen,
+        ):
+            with NamedTemporaryFile(suffix=".pdf", delete=False) as temp_file:
+                temp_file.close()
+                document_to_text(filename=temp_file.name, config=self.config)
+
+        expected_calls = [
+            mock.call(
+                (
+                    f"{self.empty_dir}/pdftotext",
+                    temp_file.name,
+                    "-",
+                ),
+                stdout=subprocess.PIPE,
+            ),
+        ]
+        self.mock_popen.assert_has_calls(expected_calls)
