RudolfCardinal/pythonlib

commit bdc9983e1cb1e2cf07783284903cab1a789a2b9e

Workaround BeautifulSoup not handling empty byte array correctly

cardinal_pythonlib/extract_text.py
@@ -1140,6 +1140,12 @@ def convert_html_to_text(
     """
     Converts HTML to text.
     """
+
+    # beautifulsoup4==4.13.4 returns "b''" for an empty bytes array
+    # So we just workaround this here:
+    if bytes is not None and len(blob) == 0:
+        return ""
+
     with get_filelikeobject(filename, blob) as fp:
         soup = bs4.BeautifulSoup(fp, "html.parser")
         return soup.get_text()

commit 4f25d3fbd30734cff99dd1dbc73bb6ab9e554915

Fix silly mistake introduced in bdc9983e1cb1e2cf07783284903cab1a789a2b9e

cardinal_pythonlib/extract_text.py
@@ -1148,7 +1148,7 @@ def convert_html_to_text(
     # https://bugs.launchpad.net/beautifulsoup/+bug/2110492
     # beautifulsoup4==4.13.4 returns "b''" for an empty bytes array
     # So we just workaround this here:
-    if bytes is not None and len(blob) == 0:
+    if blob is not None and len(blob) == 0:
         return ""
 
     with get_filelikeobject(filename, blob) as fp:

cardinal_pythonlib/tests/extract_text_tests.py
@@ -217,6 +217,26 @@ class DocumentToTextTests(ExtractTextTestCase):
         )
         self.assertEqual(text.strip(), content)
 
+    def test_htm_file_converted(self) -> None:
+        content = self.fake.paragraph(nb_sentences=10)
+
+        html = f"""
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+{content}
+</body>
+</html>
+"""
+        with NamedTemporaryFile(suffix=".htm", delete=False) as temp_file:
+            temp_file.write(html.encode("utf-8"))
+            temp_file.close()
+            text = document_to_text(filename=temp_file.name)
+
+        self.assertEqual(text.strip(), content)
+
     def test_empty_htm_converted(self) -> None:
         text = document_to_text(
             blob="".encode("utf-8"), extension="htm", config=self.config